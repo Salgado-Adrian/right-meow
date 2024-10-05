94af86af686e5dd42930ec8ce511b0e43ca60d04
Beasts <adrians1209@hotmail.com>
2024-10-05 12:35:15 -0400

    new frontend design

--- cat-adoption-frontend/src/App.js
+++ cat-adoption-frontend/src/App.js
@@ -6,8 +6,13 @@ function App() {
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100"> {/* Centering and background color */}
-            <h1 className="text-8xl font-bold mb-8">Right Meow</h1> {/* Title with Tailwind styles */}
-            <h2 className="text-1xl font-semibold m-4 mr-16 ml-16">Right Meow is a fun, interactive cat adoption website inspired by popular matchmaking applications. It matches users to their perfect feline companion based on zodiac sign compatibility. The best search tool for meows in you area</h2>
+        <div className='absolute top-1 bg-blue-500 text-white p-2 rounded hover:bg-green-600'> {/* Styled div */}
+            <h1 className="text-8xl font-bold mb-8">Right Meow Adoption</h1> {/* Title with Tailwind styles */}
+            </div>
+            <h2 className='text-3xl font-semibold m-4 mr-10 ml-10 text-left whitespace-break-spaces'>Right Meow is a fun, interactive cat adoption website inspired by popular matchmaking applications. 
+                It matches users to their perfect feline companion based on zodiac sign compatibility. 
+                </h2>
+                <h1 className='text-center text-5xl semi-bold font-mono tracking-tighter'>The best search tool for meows in your area</h1>
             <input 
                 type="text" 
                 value={zodiacSign} 
